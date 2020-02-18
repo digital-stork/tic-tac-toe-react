6e5271c0ec20d61ae244b85fdaa1012a619c0194
mohammed.regragui <mohammed.regragui@digitalstork.be>
2020-02-18 14:49:49 +0100

    Add somme TDD

--- src/__tests__/gameTest.js
+++ src/__tests__/gameTest.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import Game from '../game';
-import Board from '../board';
-import {shallow} from 'enzyme';
+import React from "react";
+import Game from "../game";
+import Board from "../board";
+import { mount } from "enzyme";
+import Square from "../square";
 
-describe('Game', () => {
-    it('should have a Board', () => {
-        const game = shallow(<Game />);
+describe("Game", () => {
+  const wrapper = mount(<Game />);
 
-        expect(game.find(Board).length).toBe(1);
-    });
+  it("should have a Board", () => {
+    expect(wrapper.find(Board).length).toBe(1);
+  });
+
+  it("should have a grid of 9 squares", () => {
+    expect(wrapper.find(".square").length).toBe(9);
+  });
+
+  it("should X always goes first", () => {});
+
+  it("should Players cannot play on a played position", () => {});
+
+  it("should switch player untill one player has three in a row, horizontally, vertically or diagonally", () => {});
+
+  it("should game over if one player has three in a row", () => {});
+
+  it("should display the restart button if all nine squares are filled and neither player has three in a row, ", () => {});
 });
